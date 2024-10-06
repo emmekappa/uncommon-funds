c0186c2548e0737639533c7e3864532236a2ee6d
emmekappa <emmekappa@gmail.com>
2024-10-06 15:16:48 +0200

    feat: moving csv and json files to quotations/ folder

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import teleborsa_fetcher
 
-teleborsa_fetcher.fetcher("https://www.teleborsa.it/obbligazioni-titoli-di-stato/btp-italia-nv28-eur-it0005517195-it0005517195-SVQwMDA1NTE3MTk1/grafico", "IT0005517195.csv")
-teleborsa_fetcher.fetcher("https://www.teleborsa.it/fondi/cardif-vita-valorplus-carvapl-RkMuQ0FSVkFQTA/grafico", "cardif_vita_valorplus.csv")
+teleborsa_fetcher.fetcher("https://www.teleborsa.it/obbligazioni-titoli-di-stato/btp-italia-nv28-eur-it0005517195-it0005517195-SVQwMDA1NTE3MTk1/grafico", "quotations/IT0005517195")
+teleborsa_fetcher.fetcher("https://www.teleborsa.it/fondi/cardif-vita-valorplus-carvapl-RkMuQ0FSVkFQTA/grafico", "quotations/cardif_vita_valorplus")
