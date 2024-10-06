61aaea3c28a30792134cc7dc63c1ebf8be82fc48
emmekappa <emmekappa@gmail.com>
2024-10-06 02:04:59 +0200

    feat: adding another ISIN

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 import teleborsa_fetcher
 
-teleborsa_fetcher.fetcher("cardif-vita-valorplus-carvapl-RkMuQ0FSVkFQTA", "cardif_vita_valorplus.csv")
+teleborsa_fetcher.fetcher("https://www.teleborsa.it/obbligazioni-titoli-di-stato/btp-italia-nv28-eur-it0005517195-it0005517195-SVQwMDA1NTE3MTk1/grafico", "IT0005517195.csv")
+teleborsa_fetcher.fetcher("https://www.teleborsa.it/fondi/cardif-vita-valorplus-carvapl-RkMuQ0FSVkFQTA/grafico", "cardif_vita_valorplus.csv")
--- teleborsa_fetcher.py
+++ teleborsa_fetcher.py
@@ -4,29 +4,31 @@ from datetime import datetime
 import pandas as pd
 import json
 import os
+import sys
 
-def fetcher(ticker: str, filename: str) -> None:
-
-    url = f"https://www.teleborsa.it/fondi/{ticker}/grafico"
+def fetcher(url: str, filename: str) -> None:
+    sys.stdout.write(f"Fetching data from {url} and saving to {filename}... ")
+    if not url.endswith("/grafico"):
+        raise Exception("The url must end with /grafico")
     response = requests.get(url)
 
     if response.status_code == 200:
         page_content = response.text
     else:
-        raise Exception(f"Errore nel download della pagina web: {response.status_code}")
+        raise Exception(f"Error while fetching URL: {response.status_code}")
 
     pattern_snippet = r'historical:(\[\[.*\]\])'
     matches = re.findall(pattern_snippet, page_content, re.DOTALL | re.MULTILINE)
 
 
     if not matches:
-        raise Exception("Non è stato trovato lo snippet di codice con i dati storici")
+        raise Exception("Cannot find historical data in the page")
 
     entries = json.loads(matches[0])
 
     data = []
     for entry in entries:
-        timestamp = int(entry[0]) / 1000  # Convertire da millisecondi a secondi
+        timestamp = int(entry[0]) / 1000
         date = datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d')
         price = float(entry[1])
         data.append((date, price))
@@ -40,3 +42,4 @@ def fetcher(ticker: str, filename: str) -> None:
         combined_df = new_df
 
     combined_df.to_csv(filename, index=False)
+    sys.stdout.write("Done.\n")
