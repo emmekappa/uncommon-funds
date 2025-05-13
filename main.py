d2dd1fb60547575433a71e4e87ff5084f953de51
emmekappa <emmekappa@gmail.com>
2025-05-13 19:58:47 +0200

    fix: add call for ES0000012F76 quotation

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ import teleborsa_fetcher
 
 teleborsa_fetcher.fetcher("https://www.teleborsa.it/obbligazioni-titoli-di-stato/btp-italia-nv28-eur-it0005517195-it0005517195-SVQwMDA1NTE3MTk1/grafico", "quotations/IT0005517195")
 teleborsa_fetcher.fetcher("https://www.teleborsa.it/fondi/cardif-vita-valorplus-carvapl-RkMuQ0FSVkFQTA/grafico", "quotations/cardif_vita_valorplus")
+teleborsa_fetcher.fetcher("https://www.teleborsa.it/obbligazioni/obligaciones-tf-0-5percent-ap30-eur-es0000012f76-es0000012f76-RVMwMDAwMDEyRjc2/grafico", "quotations/ES0000012F76")
